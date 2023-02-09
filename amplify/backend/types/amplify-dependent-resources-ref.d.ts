e4364f6172720bb147a908b4e635ba8ea52096d1
Isaiah Collins Abetong <isaiahcollins_02@live.com>
2023-02-09 11:00:05 +0800

    First Commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "kebab427045ca": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
+import { Amplify } from 'aws-amplify'
+import { Authenticator } from '@aws-amplify/ui-react'
+import config from '@/aws-exports'
 import '@/styles/globals.css'
+import '@aws-amplify/ui-react/styles.css'
 import type { AppProps } from 'next/app'
 
+Amplify.configure({ ...config, ssr: true })
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Authenticator>
+      <Component {...pageProps} />
+    </Authenticator>
+  )
 }
