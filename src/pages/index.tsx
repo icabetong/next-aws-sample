a7ca2cb39483675a2c9cd14b752a2494a0deba8c
Isaiah Collins Abetong <isaiahcollins_02@live.com>
2023-02-13 15:07:31 +0800

    some forms

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import Head from 'next/head'
 import { Auth } from 'aws-amplify'
-import { Button, Flex } from '@aws-amplify/ui-react'
+import {
+  Button,
+  Card,
+  Flex,
+  Heading,
+  TextField,
+  SelectField,
+} from '@aws-amplify/ui-react'
 
 export default function Home() {
-  const handleSignOut = () => Auth.signOut()
-
   return (
     <>
       <Head>
@@ -14,11 +19,45 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div className="flex items-center p-4">
-          <Button variation="primary" onClick={handleSignOut}>
-            Sign Out
-          </Button>
-          Main Page
+        <div className="p-4">
+          <Card width="relative.full" padding="xl">
+            <Heading marginBottom="medium">Checkout</Heading>
+            <TextField label="Name" placeholder="Joachim von Ribbentrop" />
+            <Heading marginTop="medium" marginBottom="small">
+              Address
+            </Heading>
+            <Flex>
+              <TextField
+                label="House/Apt. No."
+                placeholder="1600 Wilhelm Staße"
+              />
+              <TextField label="Barangay" placeholder="Mittelplatz" />
+              <TextField label="City" placeholder="Germania" />
+            </Flex>
+            <Flex>
+              <SelectField label="Province" placeholder="Select">
+                <option value="brandenburg">Brandenburg</option>
+                <option value="hessen">Hessen</option>
+                <option value="wesser-ems">Wesser-Ems</option>
+                <option value="bayern">Bayern</option>
+              </SelectField>
+              <TextField
+                label="ZIP/Postal Code"
+                type="number"
+                placeholder="XXXX"
+              />
+            </Flex>
+            <Heading marginTop="medium" marginBottom="small">
+              Billing Details
+            </Heading>
+            <SelectField label="Card">
+              <option value="bank1">Deutsche Bank - VISA</option>
+              <option value="bank2">Bank of England</option>
+            </SelectField>
+            <Button marginTop="large" variation="primary" isFullWidth>
+              Place Order
+            </Button>
+          </Card>
         </div>
       </main>
     </>
